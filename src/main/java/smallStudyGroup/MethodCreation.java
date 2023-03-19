acdf938ba3058ecb837d9c7798db64cfe625921d
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2023-03-19 12:02:09 +0300

    01TR

---
+++ src/main/java/smallStudyGroup/MethodCreation.java
@@ -0,0 +1,4 @@
+package dailypractices.smallStudyGroup;
+
+public class MethodCreation {
+}
---
+++ src/main/java/smallStudyGroup/WrapperClass.java
@@ -0,0 +1,17 @@
+package dailypractices.smallStudyGroup;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+
+        boolean ogrenciMi = true;
+
+        Boolean wOgrenciMi = ogrenciMi;
+
+        System.out.println(wOgrenciMi);
+
+
+
+    }
+
+
+}
